a349da54db42451efdeed8b2e9c725d61dbfec52
KlaudiaZ <klaudia.zajac@email.com>
2018-04-11 20:49:03 +0200

    preparations and bulma install

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import  './css/index.css';
-import  './css/other.css';
+import './css/index.css';
+import './css/other.css';
+import 'bulma/css/bulma.css';
 console.log('JavaScript was attached to the page!');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Locked and loaded!');
+});
