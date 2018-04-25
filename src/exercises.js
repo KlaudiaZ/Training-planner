f070932d81bf5fe7466e26defc9e34a034611622
KlaudiaZ <klaudia.zajac@email.com>
2018-04-25 21:48:36 +0200

    Styled and scripted 'new exercise' creation form

--- src/exercises.js
+++ src/exercises.js
@@ -1,20 +1,27 @@
 import $ from 'jquery';
 
-export const bindBackButtonOnExercises = () => {
+// clear form after adding exercise
+// validate inputs
+// attach 'kg' to 'weight' field
+// write N/A if 'weight' is left empty
+// check for the highest id number at the beginning
+// remove the form from html, make it appear solely through JS
+
+export const bindBackButtonOnExercisesMenu = () => {
     $('#go-back-exercises').click((e) => {
         $('#main-menu').attr('data-visibility', 'visible');
         $('#exercises-list').attr('data-visibility', 'invisible');
     });
 }
 
-export const bindAddButtonOnExercises = () => {
-    $('#add-exercise').click((e) => {
+export const bindAddButtonOnExercisesMenu = () => {
+    $('#add-exercise-main').click((e) => {
         showExerciseForm();
     });
 }
 
 const createNewExercise = (id, name, series, reps, weight) => {
-    $('#exercises-list').append($(`<div class="exercise" id="exercise-"${id}`))
+    $('#exercises-list').append($(`<div class="exercise" id="exercise-${id}">`)
         .html(`<div class="name row">
         <p>${name}</p>
         </div>
@@ -31,9 +38,35 @@ const createNewExercise = (id, name, series, reps, weight) => {
         <button class="button main-button">
         <img src="img/plus.png">
         </button>
-        </div>`)
+        </div>`))
 }
 
 const showExerciseForm = () => {
+    $('#new-exercise-form').attr('data-visibility', 'visible');
+    bindAreaAroundForm();
+    bindAddExerciseButton();
+}
 
+const bindAreaAroundForm = () => {
+    $('#new-exercise-form').click((e) => {
+        if (e.target === e.currentTarget) {
+            hideExerciseForm();
+        }
+    });
+}
+
+const hideExerciseForm = () => {
+    $('#new-exercise-form').attr('data-visibility', 'invisible');
+}
+
+const bindAddExerciseButton = () => {
+    $('#add-exercise-form').click((e) => {
+        addExercise();
+    });
+}
+
+const addExercise = () => {
+    console.log('your exercise should be added here');
+    createNewExercise("10" /*add id-check*/ , $('#exercise-name').val(), $('#exercise-series').val(), $('#exercise-reps').val(), $('#exercise-weight').val());
+    hideExerciseForm();
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './css/other.css';
 import $ from 'jquery';
 import { bindTrainingPlansButton, bindExercisesButton } from './mainMenu';
 import { bindBackButtonOnPlans } from './plans';
-import { bindBackButtonOnExercises, bindAddButtonOnExercises } from './exercises';
+import { bindBackButtonOnExercisesMenu, bindAddButtonOnExercisesMenu } from './exercises';
 console.log('JavaScript was attached to the page!');
 
 $(() => {
@@ -11,6 +11,6 @@ $(() => {
     bindTrainingPlansButton();
     bindExercisesButton();
     bindBackButtonOnPlans();
-    bindBackButtonOnExercises();
-    bindAddButtonOnExercises();
+    bindBackButtonOnExercisesMenu();
+    bindAddButtonOnExercisesMenu();
 });
